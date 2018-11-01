3e03243236502c75408eb86237860d2ce2ef77d8
Alexandre <alexandre.mart@epitech.eu>
2018-11-01 21:51:41 +0100

    fix score

--- src/index.js
+++ src/index.js
@@ -30,11 +30,11 @@ io.on('connection', (ctx) => {
   ctx.socket.on('playerClick', async () => {
     if (await models.rooms.increaseCount({ models: models, id: ctx.roomId }) === 'destroy') {
       const score = await models.users.defeat({ userId: ctx.userId });
-      ctx.socket.emit("score", { score : score});      
+      ctx.socket.emit("score", score);      
       io.socket.to(ctx.roomId).emit("destroy", {});
     } else {
       const score =  await models.users.gainScore({ models: models, roomId: ctx.roomId, userId: ctx.userId });
-      ctx.socket.emit("score", { score : score});      
+      ctx.socket.emit("score",  score);      
     };
   })
 
